c4d13555ffd79723cc046738066a3f143ccaa460
Vicente Souza <vicentedesouza762@gmail.com>
2025-08-22 19:44:48 -0300

    ✅ PROJETO FINAL: CRUD Gamificado completo e funcionando

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-ï»¿const express = require('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Servir arquivos estáticos
+app.use(express.static('.'));
+
+// Rota principal
 app.get('/', (req, res) => {
-  res.send('CRUD Gamificado funcionando!');
+    res.sendFile(__dirname + '/hero_academy_frontend.html');
 });
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando na porta ' + PORT);
+    console.log('?? Servidor rodando: http://localhost:' + PORT);
 });
-
